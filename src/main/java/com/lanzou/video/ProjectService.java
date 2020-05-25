f7aac06b8c82fc97009d26f91b3c9bf7350a7fff
zhengxianxin <5201314llj>
2020-05-25 15:06:43 +0800

    添加图片

--- src/main/java/com/lanzou/video/ProjectService.java
+++ src/main/java/com/lanzou/video/ProjectService.java
@@ -160,10 +160,6 @@ public class ProjectService {
 
     }
 
-    public static void main(String[] args) {
-        ProjectService service = new ProjectService();
-        service.cutProject("E:\\BaiduNetdiskDownload\\剑指Java面试-Offer直通车");
-    }
 
 
 }
---
+++ src/main/java/com/lanzou/video/VideoDemo.java
@@ -0,0 +1,10 @@
+package com.lanzou.video;
+
+public class VideoDemo {
+
+
+    public static void main(String[] args) {
+        ProjectService service = new ProjectService();
+        service.cutProject("E:\\BaiduNetdiskDownload\\剑指Java面试-Offer直通车");
+    }
+}
